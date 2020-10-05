5dc89aeb0a04d4191fe93eb80c66cf8122fc2da7
Morteza Ansarinia <ansarinia@me.com>
2020-10-05 23:43:57 +0200

    fix BIDS-validator errors for resting task

--- notebooks/rest2bids.py
+++ notebooks/rest2bids.py
@@ -10,13 +10,15 @@ import json
 rest_dir = Path('data/julia2018_raw/Resting_State')
 bids_dir = Path('data/Julia2018BIDS/')
 dataset_description = {
-    "Name": "Julia2008 Dataset",
-    "BIDSVersion": "1.4.0",
-    "DatasetType": "raw"
+    'Name': 'Julia2008 Dataset',
+    'BIDSVersion': '1.4.0',
+    'DatasetType': 'raw',
+    'Authors': ['Julia', 'Daphne']
 }
 
 
 bids_dir.mkdir(exist_ok=True)
+
 
 # (bids_dir / 'dataset_description.json').touch(exist_ok=True)
 with open(bids_dir / 'dataset_description.json', 'w') as f:
@@ -28,7 +30,7 @@ subjects = [x.stem for x in rest_dir.iterdir() if x.is_dir()]
 
 for subj in subjects:
 
-  print(f'>>> Converting {subj} data into BIDS...')
+  print(f'> Converting {subj} data into BIDS...')
 
   # Initialize folder structure
   for mod in ['anat', 'func']:
@@ -47,7 +49,21 @@ for subj in subjects:
       f'sub-{subj}_ses-3_task-rest_bold.nii.gz'
   shutil.copyfile(rest_bold, bids_rest_bold)
 
-  # TODO: use dcm2niix or dicom2nifti to convert DICOMs into Nifti+Sidecar 
+  # func/rest-bold sidecar
+  # TODO: use nibabeldicom2nifti instead to convert DICOMs into Nifti+Sidecars
+
+  rest_bold_sidecar = {
+      'TaskName': 'rest',
+      'RepetitionTime': 3.0
+  }
+
+  rest_bold_sidecar_path = bids_dir / f'sub-{subj}' / 'ses-3' / 'func' / \
+      f'sub-{subj}_ses-3_task-rest_bold.json'
+
+  with open(rest_bold_sidecar_path, 'w') as f:
+    json.dump(rest_bold_sidecar, f, indent=2)
+    f.write('\n')
+
 
 print('Done!')
 print(f'BIDS-ready folder: {str(bids_dir.absolute())}')
