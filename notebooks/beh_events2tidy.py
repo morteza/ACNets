c5ecce2cbc9f04f7d20f6aea2e1a287bccc7308e
Morteza Ansarinia <ansarinia@me.com>
2020-10-05 16:35:10 +0200

    full beh_L1 according to the analysis plan (see Appendix A)

--- notebooks/beh_events2tidy.py
+++ notebooks/beh_events2tidy.py
@@ -54,11 +54,17 @@ def get_trial(events):
 
   cue = cue_events.type.apply(lambda s: s.split(' ')[1]).values[0]
   cue_ts = cue_events.realTime.iloc[0]
+  cue_duration = None
+  if len(stimulus_events) > 0:
+    cue_duration = \
+        stimulus_events.realTime.iloc[0] - cue_events.realTime.iloc[0]
 
-  stimulus = \
-      stimulus_events.type.apply(lambda s: s.split(' ')[1]).values[0] \
-      if len(stimulus_events) > 0 else None
-  stimulus_ts = stimulus_events.realTime.iloc[0]
+  stimulus = None
+  stimulus_ts = None
+  stimulus_duration = None  # TODO calc stimulus_duration
+  if len(stimulus_events) > 0:
+    stimulus = stimulus_events.type.apply(lambda s: s.split(' ')[1]).values[0]
+    stimulus_ts = stimulus_events.realTime.iloc[0]
 
   response = None
   response_ts = None
@@ -71,27 +77,59 @@ def get_trial(events):
       'trial_index': trial_index,
       'cue': cue,
       'stimulus': stimulus,
+      'stimulus_timestamp': stimulus_ts,
+      'stimulus_duration': stimulus_duration,
       'response': response,
       'cue_timestamp': cue_ts,
-      'stimulus_timestamp': stimulus_ts,
+      'cue_duration': cue_duration,
       'response_timestamp': response_ts
   })
 
 
-for csv_file in Path(input_dir).glob('**/NVGP07_A1_events.csv'):
+# main loop over csv files
+for csv_file in Path(input_dir).glob('**/*_events.csv'):
 
-  participant_id, session_index = re.search('([^_]+)_(.+)_events',
-                                            csv_file.stem,
-                                            re.IGNORECASE).groups()
+  participant_id, session = re.search('([^_]+)_(.+)_events',
+                                      csv_file.stem).groups()
 
-  print(f'parsing {participant_id} (Session {session_index})...')
+  participant_group = re.search('([A-Z]+).*',
+                                participant_id).group(1)
 
-  output_path = Path(output_dir).joinpath(csv_file.stem + '.csv')
+  print(f'>>> parsing {participant_id} (Session {session})...')
+
+  output_path = Path(output_dir).joinpath(
+      csv_file.stem.replace('_events', '_tidy.csv')
+  )
 
   TRIAL_PARAMS = pd.read_csv(str(csv_file).replace('_events', '_trials'))
+  TRIAL_PARAMS['participant_id'] = participant_id
+  TRIAL_PARAMS['group'] = participant_group
   TRIAL_PARAMS['trial_index'] = TRIAL_PARAMS.index + 1
+  TRIAL_PARAMS['session'] = session
+  TRIAL_PARAMS['has_missing_arms'] = TRIAL_PARAMS.missingArms > 0
+  TRIAL_PARAMS.replace({'type': {
+      'standardvalid': 'standard_valid',
+      'standardinvalid': 'standard_invalid',
+      'deviantvalid': 'distractor_valid',
+      'deviantinvalid': 'distractor_invalid',
+      'cue': 'catch'
+  }}, inplace=True)
 
+  TRIAL_PARAMS.rename({
+      'type': 'trial_type',
+      'trialTime': 'trial_duration',
+      'ITI': 'ITI_duration',
+      'SOA': 'SOA_duration',
+      'deviant': 'distractor',
+      'missingArms': 'missing_arms_n'
+  }, axis=1, inplace=True)
+
+  TRIAL_PARAMS['stimulus_contrast'] = None  # TODO extract stimulus_contrast
+  TRIAL_PARAMS.drop(columns=['cue', 'gabor'], inplace=True)
+
+  # now read and parse events; then extract trial data
   EVENTS = pd.read_csv(str(csv_file))
+
   EVENTS.sort_values(by='realTime', inplace=True)
 
   EVENTS['block_index'] = EVENTS.type.apply(get_block_index).ffill()
@@ -105,8 +143,37 @@ for csv_file in Path(input_dir).glob('**/NVGP07_A1_events.csv'):
                                       .ffill())
 
   TRIALS = EVENTS.groupby(['trial_index'], as_index=False).apply(get_trial)
+
   TRIALS['rt'] = TRIALS.response_timestamp - TRIALS.stimulus_timestamp
   TRIALS['correct'] = (TRIALS.stimulus == TRIALS.response) & (TRIALS.rt > 0)
+  TRIALS['preparation_duration'] = \
+      TRIALS.stimulus_timestamp - TRIALS.cue_timestamp
 
   TRIALS = TRIALS.merge(TRIAL_PARAMS, on='trial_index')
+
+  # re-oreder columns to match UML diagram in the analysis plan
+  TRIALS = TRIALS[[
+      'participant_id',
+      'group',
+      'session',
+      'block_index',
+      'trial_index',
+      'trial_type',
+      'cue',
+      'stimulus',
+      'stimulus_contrast',
+      'missing_arms_n',
+      'response',
+      'rt',
+      'correct',
+      'SOA_duration',
+      'ITI_duration',
+      'cue_duration',
+      'preparation_duration',
+      'stimulus_duration',
+      'cue_timestamp',
+      'stimulus_timestamp',
+      'response_timestamp'
+  ]]
+
   TRIALS.to_csv(output_path)
