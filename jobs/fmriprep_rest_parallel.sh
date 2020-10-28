1d027c4e5919daa1a00850e77a3cada8c652eb50
Morteza Ansarinia <ansarinia@me.com>
2020-10-28 11:53:31 +0100

    added a new hpc job to group participants and prep them in 10 parallel jobs

---
+++ jobs/fmriprep_rest_parallel.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#SBATCH --job-name=fmriprep_rest_parallel
+#SBATCH --array=0-9
+#SBATCH --output=/work/projects/acnets/logs/fmriprep_rest.%A_%a.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24gb
+#SBATCH --partition=batch
+#SBATCH --time=1-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=morteza.ansarinia@uni.lu
+
+
+JOB_NAME=fmriprep_rest
+DATASET=julia2018_datalad_v2020.10.2
+RANDOM_SEED=42
+
+PROJECT_DIR=/work/projects/acnets/
+INPUT_DIR=${SCRATCH}${DATASET}/bids
+OUTPUT_DIR=${PROJECT_DIR}derivatives/$JOB_NAME
+TMP_WORK_DIR=${SCRATCH}fmriprep_work
+
+# Subjects with resting bold and t1w
+SUBJECTS_GROUPS=("AVGP01 AVGP02 AVGP03" "AVGP04 AVGP05 AVGP08" "AVGP10 AVGP12NEW AVGP13" "AVGP13NEW AVGP14 AVGP14NEW" "AVGP16NEW AVGP17NEW AVGP18 AVGP20")
+SUBJECTS_GROUPS+=("NVGP01 NVGP03 NVGP04" "NVGP06 NVGP07 NVGP08" "NVGP10 NVGP12 NVGP13" "NVGP14 NVGP15 NVGP16" "NVGP17 NVGP17NEW NVGP19 NVGP19NEW")
+
+
+# Choose subject based on slurm array id
+SUBJECTS=${SUBJECTS_GROUPS[$SLURM_ARRAY_TASK_ID]}
+
+echo "Slurm job ID: " $SLURM_JOB_ID
+echo "Subjects: " $SUBJECTS
+
+# enable access to the `module` cli (HPC 2019: tools/Singularity/3.6.0)
+module purge
+module load tools/Singularity
+
+# prepare dataset and work dir
+if [ ! -d $SCRATCH/$DATASET ]; then
+    tar xjf ${PROJECT_DIR}backup/$DATASET.tar.bz2 -C $SCRATCH
+fi
+
+# create temp working dir
+mkdir -p $TMP_WORK_DIR
+mkdir -p $OUTPUT_DIR
+
+
+# to avoid unexpected results, only create fmriprep SIF if it does not already exist
+if [ ! -f ${SCRATCH}fmriprep_latest.simg ]; then
+    singularity build \
+        ${SCRATCH}fmriprep_latest.simg \
+        docker://poldracklab/fmriprep:latest
+fi
+
+
+# run fmriprep
+singularity run --cleanenv \
+    -B $INPUT_DIR:/inputs \
+    -B $OUTPUT_DIR:/outputs \
+    -B $SCRATCH \
+    $SCRATCH/fmriprep_latest.simg \
+    --mem 24GB --n-cpus 8 \
+    --fs-license-file $HOME/freesurfer_license.txt \
+    --work-dir $TMP_WORK_DIR \
+    --notrack \
+    --skull-strip-t1w skip \
+    --write-graph \
+    --random-seed $RANDOM_SEED \
+    /inputs /outputs participant \
+    --participant-label $SUBJECTS
