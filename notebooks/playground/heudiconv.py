05829a3066163759faeb525e4218112a736995ac
Morteza Ansarinia <ansarinia@me.com>
2020-11-23 13:25:33 +0100

    single-subject tfmri preprocessing

--- notebooks/playground/heudiconv.py
+++ notebooks/playground/heudiconv.py
@@ -12,20 +12,20 @@ print('working directory:', os.getcwd())
 raw_data_path = Path('data/julia2018_raw/RawDicom_A1_A2/Attention')
 
 jsubs = [p.stem for p in raw_data_path.glob('*VGP*')]
+jsubs = ['NVGP01']
 sessions = ['1', '2']
 
 
 for ses in sessions:
   for jsub in jsubs:
-    cmd = ("heudiconv"
-           " --bids"
-           " -f python/preprocessing/heuristic.py"
-           f" --files {raw_data_path}/{jsub}/RawDICOM/Attention{ses}"
-           f" -s {jsub}"
-           f" -ss {ses}"
-           " -o data/julia2018_raw_task"
-           " -c dcm2niix"
-           " --datalad")
+    cmd = ('heudiconv'
+           ' --bids'
+           ' -f python/preprocessing/heuristic.py'
+           f' --files {raw_data_path}/{jsub}/RawDICOM/Attention{ses}'
+           f' -s {jsub}'
+           f' -ss {ses}'
+           ' -o data/julia2018_tfmri_bids'
+           ' -c dcm2niix')
     print(cmd)
 
     res = subprocess.run(cmd.split(' '), stderr=subprocess.PIPE, text=True, cwd=os.getcwd())
