4d1810d31c3beea7ebde94c7d09db074728be20c
Morteza Ansarinia <ansarinia@me.com>
2022-03-08 15:20:35 +0100

    better DiFuMu atlas handling

--- python/acnets/parcellations/difumo.py
+++ python/acnets/parcellations/difumo.py
@@ -1,12 +1,14 @@
+import re
 import pandas as pd
 from nilearn import datasets, plotting, maskers
 
 
-def load_masker(atlas_name, mask_img):
-  _, dim, res = atlas_name.split('_')
+def load_masker(atlas_name: str, mask_img):
+  dim, res = re.findall('difumo_(\\d{2,4})_(\\d)mm', atlas_name.lower())[0]
+
   atlas = datasets.fetch_atlas_difumo(
-      dimension=dim,
-      resolution_mm=res,
+      dimension=int(dim),
+      resolution_mm=int(res),
       legacy_format=False)
 
   masker = maskers.NiftiMapsMasker(
@@ -20,9 +22,9 @@ def load_masker(atlas_name, mask_img):
       plotting.find_probabilistic_atlas_cut_coords(maps_img=atlas.maps))
   atlas_labels = pd.concat([atlas.labels, pd.DataFrame(atlas_coordinates)], axis=1)
   atlas_labels.rename(columns={
-      0: 'mni152_x',
-      1: 'mni152_y',
-      2: 'mni152_z'}, inplace=True)
+      0: 'x',
+      1: 'y',
+      2: 'z'}, inplace=True)
 
   atlas_labels.drop(columns=['component'], inplace=True)
   atlas_labels.index.name = 'region'
