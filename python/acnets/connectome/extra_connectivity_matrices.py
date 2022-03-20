02d55f3b9dd810ee5737984bb3bdfa00e1fd8ce1
Morteza Ansarinia <ansarinia@me.com>
2022-03-20 11:47:30 +0100

    optimize time taken to import pipeline and re-organize notebooks

--- python/acnets/connectome/extra_connectivity_matrices.py
+++ python/acnets/connectome/extra_connectivity_matrices.py
@@ -1,10 +1,5 @@
-import numpy as np
 from sklearn.covariance import LedoitWolf
 from nilearn import connectome
-
-from ._transfer_entropy import transfer_entropy
-from ._chatterjee import chatterjee_xicoef
-
 
 class ExtraConnectivityMeasure(connectome.ConnectivityMeasure):
 
@@ -27,25 +22,17 @@ class ExtraConnectivityMeasure(connectome.ConnectivityMeasure):
       super().__init__(cov_estimator, kind, vectorize, discard_diagonal)
 
   def fit(self, X, y=None):
-    if self.kind == 'transfer_entropy':
-      return self
-    elif self.kind == 'chatterjee':
+    if self.kind in ['transfer_entropy', 'chatterjee']:
       return self
     else:
       return super().fit(X, y)
 
-  def transform(self, X, confounds=None):
+  def transform(self, X, y=None, confounds=None):
     if self.kind == 'transfer_entropy':
-      return transfer_entropy(X)
-    elif self.kind == 'chatterjee':
-      return chatterjee_xicoef(X)
-    else:
-      return super().transform(X, confounds)
-
-  def fit_transform(self, X, y=None, confounds=None):
-    if self.kind == 'transfer_entropy':
+      from ._transfer_entropy import transfer_entropy
       return transfer_entropy(X)
     if self.kind == 'chatterjee':
+      from ._chatterjee import chatterjee_xicoef
       return chatterjee_xicoef(X)
     else:
       return super().fit_transform(X, y, confounds)
