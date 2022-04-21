eaa24ce4a77e6288f11efe77e5570ab534771b38
Morteza Ansarinia <ansarinia@me.com>
2022-04-21 11:55:06 +0200

    refactor pipeline and visualizations

--- python/acnets/pipeline/__init__.py
+++ python/acnets/pipeline/__init__.py
@@ -2,10 +2,12 @@ from .parcellation import Parcellation
 from .connectivity_vectorizer import ConnectivityVectorizer
 from .connectivity_extractor import ConnectivityExtractor
 from .network_aggregator import NetworkAggregator
+from .connectivity import Connectivity
 
 
 __all__ = ['Parcellation',
            'ConnectivityVectorizer',
            'ConnectivityExtractor',
-           'NetworkAggregator'
+           'NetworkAggregator',
+           'Connectivity'
            ]
