40883cd30878784ef2e2a63d0b6ca97d290da349
morteza <ansarinia@me.com>
2023-03-10 12:42:39 +0100

    faster cerebellum classifier; seems like the values for max_features in the SelectFromModel impacts the accuracy

--- src/acnets/pipeline/utils.py
+++ src/acnets/pipeline/utils.py
@@ -1,4 +1,7 @@
 import logging
+from joblib import Parallel
+from sklearn import model_selection
+from tqdm import tqdm
 
 
 def get_tr(self, default=3.0):
@@ -21,3 +24,26 @@ def get_tr(self, default=3.0):
     logging.warn('Error occurred while retrieving TR from the BIDS dataset.', e)
     return default
 
+
+class GridSearchCVProgressBar(model_selection.GridSearchCV):
+    """Show progress bar in grid search (Borrowed from `pactools/grid_search.py`)."""
+
+    def _get_param_iterator(self):
+
+        iterator = super(GridSearchCVProgressBar, self)._get_param_iterator()
+        iterator = list(iterator)
+        n_candidates = len(iterator)
+
+        cv = model_selection._split.check_cv(self.cv, None)
+        n_splits = getattr(cv, 'n_splits', 3)
+        max_value = n_candidates * n_splits
+
+        class ParallelProgressBar(Parallel):
+            def __call__(self, iterable):
+                bar = tqdm(total=max_value, title='GridSearchCV')
+                iterable = bar(iterable)
+                return super(ParallelProgressBar, self).__call__(iterable)
+
+        model_selection._search.Parallel = ParallelProgressBar
+
+        return iterator
