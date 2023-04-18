f036337bc8022f31fdab12ed68bc38b943243578
morteza <ansarinia@me.com>
2023-04-18 11:27:14 +0200

    cleanups

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from setuptools import find_packages
-
-setup(name='acnets',
-      version='2020.10.1',
-      packages=find_packages('python'),
-      package_dir={'': 'python'},
-      long_description=open('README.md').read(),
-      )
