a451565707aef06b6fca08add10e05d8b75e7b08
morteza <ansarinia@me.com>
2023-07-14 14:12:53 +0200

    comments + feature names

--- src/acnets/pipeline/connectivity_pipeline.py
+++ src/acnets/pipeline/connectivity_pipeline.py
@@ -4,7 +4,6 @@ from typing import Literal
 
 import numpy as np
 import pandas as pd
-import xarray as xr
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 
@@ -13,7 +12,37 @@ from . import TimeseriesAggregator, ConnectivityExtractor, Parcellation, Connect
 
 @dataclass
 class ConnectivityPipeline(TransformerMixin, BaseEstimator):
-    """Parcellate regions, aggregate networks, and extract connectivity."""
+    """Aggregation of several steps to extract connectivity from timeseries.
+
+    Steps include parcellation, aggregate timeseries, extract connectivity matrices,
+    and aggregate connectivity matrices.
+
+    Parameters
+    ----------
+    atlas : str, default='dosenbach2010'
+        Name of the atlas to use for parcellation.
+    kind : str, default='correlation'
+        Type of connectivity to extract. See `acnets.pipeline.ConnectivityExtractor`.
+    timeseries_aggregation : str, default=None
+        Strategy to aggregate timeseries. See `acnets.pipeline.TimeseriesAggregator`.
+    connectivity_aggregation : str, default=None
+        Strategy to aggregate connectivity matrices. See `acnets.pipeline.ConnectivityAggregator`.
+    bids_dir : PathLike, default='data/julia2018'
+        Path to BIDS directory.
+    parcellation_cache_dir : PathLike, default='data/julia2018/derivatives/resting_timeseries/'
+        Path to directory to cache parcellation results.
+
+    Attributes
+    ----------
+    dataset_ : xarray.DataArray
+        Dataset with timeseries and connectivity matrices.
+
+    Returns
+    -------
+    conn : np.ndarray
+        Connectivity matrices.
+
+    """
 
     atlas: str = 'dosenbach2010'
     kind: str = 'correlation'
@@ -62,7 +91,9 @@ class ConnectivityPipeline(TransformerMixin, BaseEstimator):
         # TODO: update to extract from dataset_
 
         if input_features is None:
-            input_features = self.transform(None).coords['node'].values
+            _ = self.transform(None)  # to initialize self.dataset_ if not already
+            node_type = self.dataset_['connectivity'].dims[-1]
+            input_features = self.dataset_.coords[node_type].values
 
         feature_names = pd.DataFrame(
             np.zeros((input_features.shape[0], input_features.shape[0])),
