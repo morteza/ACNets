8541a74e47f7756b92b9ec3624ef6d8108822aec
Morteza Ansarinia <ansarinia@me.com>
2024-01-03 12:51:32 +0100

    complete deep learning baseline (~75% dropout accuracy during test given network connectivity as input)

--- src/acnets/deep/data.py
+++ src/acnets/deep/data.py
@@ -5,9 +5,22 @@ from torch.utils.data import random_split, DataLoader, TensorDataset, ConcatData
 
 from sklearn.preprocessing import LabelEncoder
 from ..pipeline import Parcellation, ConnectivityExtractor, TimeseriesAggregator, ConnectivityAggregator
+from sklearn.model_selection import train_test_split
 
 
 class ACNetsDataModule(pl.LightningDataModule):
+    """_summary_
+
+    Data
+    ----
+    - h1: time-series (regions x timepoints)
+    - h2: connectivity (regions x regions)
+    - h3: time-series (networks x timepoints)
+    - h4: connectivity (networks x networks)
+    - h5: connectivity (networks x networks)
+
+
+    """
     def __init__(self,
                  atlas='dosenbach2010',
                  kind='partial correlation',
@@ -25,34 +38,42 @@ class ACNetsDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
 
     def prepare_data(self):
-        h1_time_regions = Parcellation(atlas_name=self.atlas).fit_transform(X=None)
-        h2_conn_regions = ConnectivityExtractor(kind=self.kind).fit_transform(h1_time_regions)
-        h3_time_networks = TimeseriesAggregator(strategy='network').fit_transform(h1_time_regions)
-        h4_conn_networks = ConnectivityExtractor(kind=self.kind).fit_transform(h3_time_networks)
-        h5_conn_networks = ConnectivityAggregator(strategy='network').fit_transform(h2_conn_regions)
-        h1 = torch.Tensor(h1_time_regions['timeseries'].values)
-        h2 = torch.Tensor(h2_conn_regions['connectivity'].values)
-        h3 = torch.Tensor(h3_time_networks['timeseries'].values)
-        h4 = torch.Tensor(h4_conn_networks['connectivity'].values)
-        h5 = torch.Tensor(h5_conn_networks['connectivity'].values)
-
-        # TODO extract subject labels (AVGP or NVGP)
-        y = self.y_encoder.fit_transform([s[:4] for s in h1_time_regions['subject'].values])
-        y = torch.tensor(y)
-
-        data = TensorDataset(h1, h2, h3, h4, h5, y)
-        n_subjects = len(data)
-        self.train, self.val, self.test = random_split(
-            data,
-            [int(n_subjects * self.train_ratio),
-                int(n_subjects * self.val_ratio),
-                int(n_subjects * self.test_ratio)])
-
-        # TODO concat val to train
-        self.train = ConcatDataset([self.train, self.val])
+        # just calling parcellation once, so time-series will be cached
+        Parcellation(atlas_name=self.atlas).fit_transform(X=None)
 
     def setup(self, stage=None):
-        pass
+        if stage == 'fit' or stage is None:
+            h1_time_regions = Parcellation(atlas_name=self.atlas).fit_transform(X=None)
+            h2_conn_regions = ConnectivityExtractor(kind=self.kind).fit_transform(h1_time_regions)
+            h3_time_networks = TimeseriesAggregator(strategy='network').fit_transform(h1_time_regions)
+            h4_conn_networks = ConnectivityExtractor(kind=self.kind).fit_transform(h3_time_networks)
+            h5_conn_networks = ConnectivityAggregator(strategy='network').fit_transform(h2_conn_regions)
+            h1 = torch.Tensor(h1_time_regions['timeseries'].values)
+            h2 = torch.Tensor(h2_conn_regions['connectivity'].values)
+            h3 = torch.Tensor(h3_time_networks['timeseries'].values)
+            h4 = torch.Tensor(h4_conn_networks['connectivity'].values)
+            h5 = torch.Tensor(h5_conn_networks['connectivity'].values)
+
+            # extract subject labels (AVGP or NVGP)
+            y = self.y_encoder.fit_transform([s[:4] for s in h1_time_regions['subject'].values])
+            y = torch.tensor(y)
+
+            self.full_data = TensorDataset(h1, h2, h3, h4, h5, y)
+
+            # stratified split into train, val and test
+            n_subjects = len(y)
+            train_idx, test_idx = train_test_split(
+                torch.arange(n_subjects), test_size=self.test_ratio, stratify=y, shuffle=self.shuffle)
+            train_idx, val_idx = train_test_split(
+                train_idx, test_size=self.val_ratio / (1 - self.test_ratio),
+                stratify=y[train_idx], shuffle=self.shuffle)
+
+            self.train = torch.utils.data.Subset(self.full_data, train_idx)
+            self.val = torch.utils.data.Subset(self.full_data, val_idx)
+            self.test = torch.utils.data.Subset(self.full_data, test_idx)
+
+            # concat val and train for final training (we only use train and test for now)
+            self.train = ConcatDataset([self.train, self.val])
 
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.batch_size,
@@ -60,7 +81,8 @@ class ACNetsDataModule(pl.LightningDataModule):
                           persistent_workers=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val, batch_size=self.batch_size,
+        # Note this is the same as the test dataloader (change to self.val for separate validation set)
+        return DataLoader(self.test, batch_size=self.batch_size,
                           shuffle=False, num_workers=self.num_workers,
                           persistent_workers=True)
 
--- src/acnets/deep/multihead_model.py
+++ src/acnets/deep/multihead_model.py
@@ -2,41 +2,101 @@ import torch
 from torch.nn import functional as F
 from torch import nn
 import pytorch_lightning as pl
+import torchmetrics as metrics
+from torchmetrics.classification import BinaryAccuracy
 
 
 class MultiHeadModel(pl.LightningModule):
-    def __init__(self, n_timepoints, n_regions, n_networks):
+    def __init__(self, n_regions, n_networks):
         super().__init__()
-        self.h1_head = nn.RNN(n_regions, 8, batch_first=True)
+
+        self.train_accuracy = metrics.Accuracy(task='multiclass', num_classes=2)
+        self.val_accuracy = metrics.Accuracy(task='multiclass', num_classes=2)
+
+        self.h1_encoder = nn.LSTM(n_regions, 8, batch_first=True)
         self.h1_output = nn.Linear(8, 2)  # binary classification (AVGP or NVGP)
         self.h2_head = nn.Linear(1, 1)
-        self.h3_head = nn.Linear(1, 1)
-        self.h4_head = nn.Linear(1, 1)
-        self.h5_head = nn.Linear(1, 1)
+        self.h3_encoder = nn.LSTM(n_networks, 8, batch_first=True)
+        self.h3_output = nn.Linear(8, 2)
+        self.h4_head = nn.Sequential(
+            nn.Linear(n_networks * n_networks, n_networks),
+        )
+        self.h5_head = nn.Sequential(
+            nn.Linear(n_networks * n_networks, n_networks),
+        )
+
+        self.h4_h5_output = nn.Sequential(
+            nn.Linear(n_networks, n_networks),
+            nn.ReLU(),
+            nn.Dropout(.2),
+            nn.Linear(n_networks, 2),
+            nn.Sigmoid()
+        )
 
     def forward(self, h1, h2, h3, h4, h5):
-        (z1, _) = self.h1_head(h1)
-        y = self.h1_output(z1[:, -1, :])
+        # (z1, _) = self.h1_head(h1)
+        # y1 = self.h1_output(z1[:, -1, :])
+
+        # (_, (z3, _)) = self.h3_encoder(h3)
+        # y3 = self.h3_output(z3[0, ...])
+
+        h4 = h4.view(h4.shape[0], -1)
+        y4 = self.h4_head(h4)
+
+        h5 = h5.view(h5.shape[0], -1)
+        y5 = self.h5_head(h5)
+
+        y = y4 + y5
+        y = self.h4_h5_output(y)
+
         return y
 
     def training_step(self, batch, batch_idx):
         h1, h2, h3, h4, h5, y = batch
         y_hat = self(h1, h2, h3, h4, h5)
         loss = F.cross_entropy(y_hat, y)
-        self.log('train_loss', loss)
+        accuracy = self.train_accuracy(y_hat, y)
+        self.log('train/loss', loss)
+        self.log('train/accuracy', accuracy)
         return loss
+
+    def enable_dropout(self):
+        """Enables dropout during validation and test."""
+        for m in self.modules():
+            if m.__class__.__name__.startswith('Dropout'):
+                m.train()
 
     def validation_step(self, batch, batch_idx):
         h1, h2, h3, h4, h5, y = batch
         y_hat = self(h1, h2, h3, h4, h5)
         loss = F.cross_entropy(y_hat, y)
-        self.log('val_loss', loss)
+        accuracy = self.val_accuracy(y_hat, y.float())
+        dropout_accuracy = self.calculate_dropout_accuracy(batch)
+        self.log('val/loss', loss)
+        self.log('val/accuracy', accuracy)
+        self.log('val/dropout_accuracy', dropout_accuracy)
 
     def test_step(self, batch, batch_idx):
         h1, h2, h3, h4, h5, y = batch
         y_hat = self(h1, h2, h3, h4, h5)
         loss = F.cross_entropy(y_hat, y)
-        self.log('test_loss', loss)
+        accuracy = self.val_accuracy(y_hat, y)
+        dropout_accuracy = self.calculate_dropout_accuracy(batch)
+        self.log('test/loss', loss)
+        self.log('test/accuracy', accuracy)
+        self.log('test/dropout_accuracy', dropout_accuracy)
+
+    def calculate_dropout_accuracy(self, batch, n_repeats=10):
+        h1, h2, h3, h4, h5, y = batch
+        self.enable_dropout()
+
+        accuracy = 0
+        for i in range(n_repeats):
+            y_hat = self(h1, h2, h3, h4, h5)
+            accuracy += self.val_accuracy(y_hat, y)
+        accuracy /= n_repeats
+
+        return accuracy
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
