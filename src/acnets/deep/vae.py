2dc32483955b3d2b3e4e252f758afc32f1e272c4
Morteza Ansarinia <ansarinia@me.com>
2024-01-09 15:28:26 +0100

    Fix linear layer dimensions in VAE model and refactor reparametrization trick

--- src/acnets/deep/vae.py
+++ src/acnets/deep/vae.py
@@ -9,7 +9,7 @@ class Encoder(nn.Module):
         self.encode = nn.Sequential(
             nn.Linear(input_size, input_size // 2),
             nn.ReLU(),
-            nn.Linear(input_size //2, n_embeddings),
+            nn.Linear(input_size // 2, n_embeddings),
             nn.ReLU()
         )
 
@@ -22,9 +22,9 @@ class Decoder(nn.Module):
     def __init__(self, n_embeddings, n_outputs):
         super(Decoder, self).__init__()
         self.decode = nn.Sequential(
-            nn.Linear(n_embeddings, n_outputs // 2),
+            nn.Linear(n_embeddings, n_outputs * 2),
             nn.ReLU(),
-            nn.Linear(n_outputs // 2, n_outputs),
+            nn.Linear(n_outputs * 2, n_outputs),
             nn.ReLU()
         )
 
@@ -43,21 +43,26 @@ class VariationalAutoEncoder(nn.Module):
         self.mean_fc = nn.Linear(n_embeddings, n_embeddings)
         self.logvar_fc = nn.Linear(n_embeddings, n_embeddings)
 
+    def reparameterize(self, mean, logvar):
+        # re-parameterization trick
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        z = mean + eps * std
+        return z
+
     def forward(self, x):
 
         h = self.encode(x)
 
         mean = self.mean_fc(h)
-        logv = self.logvar_fc(h)
-        std = torch.exp(0.5 * logv)
+        logvar = self.logvar_fc(h)
 
         # re-parameterization trick
-        eps = torch.randn_like(std)
-        z = mean + eps * std
+        z = self.reparameterize(mean, logvar)
 
         x_recon = self.decode(z)
 
-        loss = self.loss(x, x_recon, mean, logv)
+        loss = self.loss(x, x_recon, mean, logvar)
 
         return h, x_recon, loss
 
